15fd62df3a334afc0f3687395f9c5ce9787fe386
rafa <rafalintirado50@gmail.com>
2026-02-16 00:14:09 +0100

    Arreglos CORS para implementar funcionalidad qr

--- src/main/java/com/smartchef/config/SecurityConfig.java
+++ src/main/java/com/smartchef/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
         config.addAllowedOriginPattern("*");
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization","Content-Type"));
-        config.setAllowCredentials(false);
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
